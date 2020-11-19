90fe4ec5bb5e1c39042d4b5b52532309bbc3102a
Renato Castro <renatocastro02@hotmail.com>
2020-11-19 16:57:15 -0300

    styledComonents e react-native-vector-icons

--- app/src/pages/Home/index.js
+++ app/src/pages/Home/index.js
@@ -1,8 +1,46 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { useNavigation } from '@react-navigation/native';
 import { Text } from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import {
+  Container,
+  ContentHome,
+  RowDataHome,
+  DataIcon,
+  DataHome,
+  ViewContato,
+} from './styles';
 
 const Home = () => {
-  return <Text>Home</Text>;
+  const { navigate } = useNavigation();
+
+  const handleNavigateToContact = useCallback(() => {
+    navigate('Contato');
+  }, [navigate]);
+
+  return (
+    <Container>
+      <ContentHome onPress={handleNavigateToContact}>
+        <RowDataHome>
+          <DataIcon>
+            <FontAwesome5 name="code" size={30} color="#fff" />
+          </DataIcon>
+          <DataHome>Serviço um</DataHome>
+          <ViewContato>
+            <MaterialCommunityIcons
+              name="greater-than"
+              size={30}
+              color="#fff"
+            />
+          </ViewContato>
+        </RowDataHome>
+        <RowDataHome>
+          <Text>Descrição</Text>
+        </RowDataHome>
+      </ContentHome>
+    </Container>
+  );
 };
 
 export default Home;
---
+++ app/src/pages/Home/styles.js
@@ -0,0 +1,38 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.SafeAreaView`
+  flex: 1;
+  padding: 0 15px;
+  justify-content: flex-start;
+  background-color: #fff;
+`;
+
+export const ContentHome = styled.TouchableOpacity`
+  border: 1px solid rgba(235, 177, 5, 0.5);
+  border-radius: 4px;
+  margin-top: 10px;
+  background-color: #ebb105;
+`;
+
+export const RowDataHome = styled.View`
+  padding: 10px;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+export const DataIcon = styled.Text`
+  flex-direction: column;
+`;
+
+export const DataHome = styled.Text`
+  padding: 0 5px;
+  flex-direction: column;
+  flex: 1;
+  font-size: 18px;
+  color: #555;
+`;
+
+export const ViewContato = styled.Text`
+  justify-content: flex-end;
+`;
